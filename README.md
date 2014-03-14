73f309e9e3e74b3af31e91fdeceb6a9ebd0c661a
Particle4Dev <steve@p4d-Vostro-270.(none)>
2014-03-15 03:49:54 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+> mkdir d1
+> mkdir d2
+> mkdir d3
+> mongod --replSet abc --dbpath d1 --port 27001 --oplogSize 50 --logpath log.1 --logappend --fork
+> mongod --replSet abc --dbpath d2 --port 27002 --oplogSize 50 --logpath log.2 --logappend --fork
+> mongod --replSet abc --dbpath d3 --port 27003 --oplogSize 50 --logpath log.3 --logappend --fork
+
+> var cfg = {
+	"_id" : "abc",
+	"members" : [
+		{
+			"_id" : 0,
+			"host" : "localhost:27001"
+		},
+		{
+			"_id" : 1,
+			"host" : "localhost:27002"
+		},
+		{
+			"_id" : 2,
+			"host" : "localhost:27003"
+		}
+	]
+}
+> rs.initiate(cfg)
---
+++ index.js
@@ -0,0 +1,71 @@
+var MongoClient = require('mongodb').MongoClient;
+/**
+MongoClient.connect('mongodb://localhost:27017/blog', function(err, db){
+    if (err) throw err;
+    var collection = db.collection('account');
+    /**
+    collection.insert({
+        user: 'new_one',
+        pass: 'pass'
+    }, function(error, res){
+        if(error)
+            throw error;
+        console.log(res);
+        db.close();
+    });
+    var stream = collection.find().stream();
+    stream.on('data', function(i){
+        console.log(i);
+    });
+    stream.on('end', function(){
+        db.close();
+    });
+    */
+    // update
+    /**
+    collection.update({user: 'le_hoang'}, {$set: {user: 'le_hoang2'}}, function(e, r){
+        console.log(e, r);
+        if(e) {
+            throw e;
+        }
+        if(r)
+            db.close();
+    });
+    */
+    // save
+    /**
+    collection.save({
+        _id: '1234',
+        user: 'mai lan changed',
+        pass: '0987 changed'
+    }, {
+        safe: true
+    }, function(e, r){
+        if(e)
+            throw e;
+        if(r)
+            console.log(r);
+        db.close();
+    });
+    * /
+    // remove
+    collection.remove({_id: '1234'}, function(e, r){
+        if(e) throw e;
+        console.log(r);
+        db.close();
+    });
+});
+*/
+OPLOG_COLLECTION = 'oplog.rs';
+REPLSET_COLLECTION = 'system.replset';
+
+MongoClient.connect('mongodb://localhost:27001/local', function(err, db){
+    if (err) throw err;
+    var collection = db.collection(OPLOG_COLLECTION);
+    collection.find({}, {tailable:true}).each(function(error, entry) {
+        if (error) {                                                  
+        } else {                                                                                                                                        
+            console.log('--- entry', entry)
+        }
+    })
+});
